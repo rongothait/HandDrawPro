f9607e883a907616f55d39088c836c0d07594aec
rongothait <ron.gothait@gmail.com>
2025-07-05 20:47:43 +0300

    Implement hand tracking and drawing functionality
    - Added a new module for hand tracking using MediaPipe in main_old.py.
    - Integrated drawing capabilities with a trail canvas that follows the right index finger.
    - Implemented gesture-based controls to enable/disable drawing and erase the canvas.
    - Created a separate main.py file to handle video capture and gesture recognition using the HandTracker class.
    - Established a canvas for drawing, including stroke management and mode handling based on detected gestures.
    - Added visual feedback for the current mode and finger detection status on the video feed.

--- constants.py
+++ constants.py
@@ -1,12 +1,16 @@
-DEFAULT_COLOR = (255, 255, 255)
+DEFAULT_COLOR = (50, 55, 75)
+DEFAULT_TEXT_COLOR = (50, 55, 75)
 MIN_DETECTION_CONFIDENCE = 0.7
 MIN_TRACKING_CONFIDENCE = 0.5
 
 # Map Gesture ID to mode_name
 GESTURE_TO_MODE = {
-    1 : 'draw',  # Open_palm
-    2 : 'drag',  # Closed_Fist
-    3 : 'erase', # Victory
+    3 : 'draw',  # Open_palm
+    1 : 'drag',  # Closed_Fist
+    6 : 'erase', # Victory
     0 : 'idle'   # Unkown
 }
 
+
+# paths
+GESTURE_TASK_PATH = "./Assets/gesture_recognizer.task"
--- hand_tracker.py
+++ hand_tracker.py
@@ -1,40 +1,67 @@
 import cv2 as cv
 import mediapipe as mp
+import numpy as np
+from mediapipe.tasks.python.vision import GestureRecognizer
+from mediapipe.tasks.python.vision.gesture_recognizer import GestureRecognizerResult
+from mediapipe.tasks.python.vision.gesture_recognizer import GestureRecognizerOptions
+from mediapipe.tasks.python.vision import RunningMode
 import constants
 
 class HandTracker:
-    def __init__(self, max_num_hands = 1):
-        self.hands_module = mp.solutions.hands
-        self.hands = self.hands_module.Hands(
-            static_image_mode = False,
-            max_num_hands = max_num_hands,
-            min_detection_confidence = constants.MIN_DETECTION_CONFIDENCE,
-            min_tracking_confidence = constants.MIN_TRACKING_CONFIDENCE
+    def __init__(self, model_path = constants.GESTURE_TASK_PATH):
+        self.options = GestureRecognizerOptions(
+            base_options=mp.tasks.BaseOptions(model_asset_path=model_path),
+            running_mode=RunningMode.VIDEO,
+            num_hands=1
         )
-        self.mp_draw = mp.solutions.drawing_utils
-    
+        self.recognizer = GestureRecognizer.create_from_options(self.options)
+        self.frame_idx = 0
+
     def process(self, frame):
-        image_rgb = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
-        result = self.hands.process(image_rgb)
+        rgb_image = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+
+        result: GestureRecognizerResult = self.recognizer.recognize_for_video(mp_image, self.frame_idx)
+        self.frame_idx += 1
 
         cursor = None
-        gesture_id = 0  # default to unknown
+        gesture_id = 0  # Default: Unknown
 
-
-        if result.multi_hand_landmarks:
-            hand_landmarks = result.multi_hand_landmarks[0]  # take first hand only
-            index_finger_tip = hand_landmarks.landmark[8]
-            h, w, _ = frame.shape
-            cursor = (int(index_finger_tip.x * w), int(index_finger_tip.y * h))
-
-            # Get gesture id if provided by the classifier
-            if result.multi_handedness:
-                classification = result.multi_handedness[0].classification[0]
-                label = classification.label  # 'Left' or 'Right'
-
+        if not result.hand_landmarks or not result.handedness:
+            return None, 0
         
-        # TODO - integrate gesture classifier
-        gesture_id = 1
+        right_idx = None
+        for i, handedness in enumerate(result.handedness):
+            if handedness[0].category_name == "Left":
+                right_idx = i
+                break
+        
+        if right_idx is None:
+            return None, 0  # No right hands detected
+        
+        hand_landmark = result.hand_landmarks[right_idx]
+        index_tip = hand_landmark[8]  # first hand, landmark 8
+        h, w, _ = frame.shape
+        cursor = (int(index_tip.x * w), int(index_tip.y * h))
+
+        if result.gestures:
+            top_gesture = result.gestures[right_idx][0]
+            label = top_gesture.category_name  # e.g. "Open_Palm"
+            score = top_gesture.score  # not in use currently
+            gesture_id = self.label_to_id(label)
+
 
         return cursor, gesture_id
-        
+
+    def label_to_id(self, label):
+        mapping = {
+            "Unknown": 0,
+            "Closed_Fist": 1,
+            "Open_Palm": 2,
+            "Pointing_Up": 3,
+            "Thumb_Down": 4,
+            "Thumb_Up": 5,
+            "Victory": 6,
+            "ILoveYou": 7
+        }
+        return mapping.get(label, 0)
